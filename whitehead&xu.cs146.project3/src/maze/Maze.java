a0b72b1516172acb9251bbe806f478b0b88cdc0e
sunnyxu2 <sunny.xu@sjsu.edu>
2019-11-07 21:58:39 -0800

    Maze generates a grid of unconnected MazeNodes

--- whitehead&xu.cs146.project3/src/maze/Maze.java
+++ whitehead&xu.cs146.project3/src/maze/Maze.java
@@ -1,9 +1,11 @@
 package maze;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
  * A maze, represented by a graph object
+ * 
  * @author Sunny
  *
  */
@@ -11,23 +13,45 @@ public class Maze {
 	private final int rows;
 	private final int cols;
 	private Random random;
+
+	private MazeNode start;
+	private MazeNode end;
 	
+	private ArrayList<MazeNode> nodes;
+
 	public Maze(int rows, int cols) {
 		this.rows = rows;
 		this.cols = cols;
-		
+
 		random = new Random();
+		
+		generateMaze();
 	}
-	
+
 	public Maze(int rows, int cols, int seed) {
 		this.rows = rows;
 		this.cols = cols;
-		
+
 		random = new Random();
 		random.setSeed(seed);
+		
+		generateMaze();
 	}
-	
-	
+
+	private void generateMaze() {
+		if (start == null || end == null) {
+			for (int i = 0; i < cols; i++) { //
+				for (int j = 0; j < rows; j++) {
+					nodes.add(new MazeNode(i, j));
+				}
+			}
+			
+			start = nodes.get(0);
+			end = nodes.get(nodes.size() - 1);
+			// implement a way to make a maze out of BFS
+		}
+	}
+
 	public String toString() {
 		return ""; // TBD
 	}
