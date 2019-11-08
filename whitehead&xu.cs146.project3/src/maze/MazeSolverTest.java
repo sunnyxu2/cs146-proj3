ad91c8efe2cdb4fadcd8aec243ae41ca3b071da7
sunnyxu2 <sunny.xu@sjsu.edu>
2019-11-07 22:00:13 -0800

    Created empty JUnit test

---
+++ whitehead&xu.cs146.project3/src/maze/MazeSolverTest.java
@@ -0,0 +1,14 @@
+package maze;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MazeSolverTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
