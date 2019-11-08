9707801aebb197f90d7ce3088851739b7fce6dc4
sunnyxu2 <sunny.xu@sjsu.edu>
2019-11-07 21:30:56 -0800

    Added 2 constructors to Maze: one with a seed and one without

--- whitehead&xu.cs146.project3/src/maze/Maze.java
+++ whitehead&xu.cs146.project3/src/maze/Maze.java
@@ -1,5 +1,33 @@
 package maze;
 
-public class Maze {
+import java.util.Random;
 
+/**
+ * A maze, represented by a graph object
+ * @author Sunny
+ *
+ */
+public class Maze {
+	private final int rows;
+	private final int cols;
+	private Random random;
+	
+	public Maze(int rows, int cols) {
+		this.rows = rows;
+		this.cols = cols;
+		
+		random = new Random();
+	}
+	
+	public Maze(int rows, int cols, int seed) {
+		this.rows = rows;
+		this.cols = cols;
+		
+		random = new Random();
+		random.setSeed(seed);
+	}
+	
+	public String toString() {
+		return ""; // TBD
+	}
 }
