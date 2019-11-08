7fa8415880fbe3fe494b64efccf788b786a4e9e5
sunnyxu2 <sunny.xu@sjsu.edu>
2019-11-07 22:07:39 -0800

    Basic constructor added

--- whitehead&xu.cs146.project3/src/maze/MazeSolver.java
+++ whitehead&xu.cs146.project3/src/maze/MazeSolver.java
@@ -1,11 +1,20 @@
 package maze;
 
-
 /**
  * Class that solves mazes
+ * 
  * @author Sunny
  *
  */
 public class MazeSolver {
-
+	private Maze maze;
+	
+	public MazeSolver(Maze maze) {
+		this.maze = maze;
+	}
+	
+	public String solve() {
+		
+		return ""; //TBD, should return toString() of 
+	}
 }
