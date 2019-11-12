c4b26fc34d131e23b3ea68f727b9333bab7931a3
sunnyxu2 <sunny.xu@sjsu.edu>
2019-11-12 11:02:18 -0800

    Started implementing maze constructor

--- whitehead&xu.cs146.project3/src/maze/Maze.java
+++ whitehead&xu.cs146.project3/src/maze/Maze.java
@@ -1,7 +1,9 @@
 package maze;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
+import java.util.Stack;
 
 /**
  * A maze, represented by a graph object
@@ -16,15 +18,15 @@ public class Maze {
 
 	private MazeNode start;
 	private MazeNode end;
-	
-	private ArrayList<MazeNode> nodes;
+
+	private MazeNode[][] nodes;
 
 	public Maze(int rows, int cols) {
 		this.rows = rows;
 		this.cols = cols;
 
 		random = new Random();
-		
+
 		generateMaze();
 	}
 
@@ -34,32 +36,85 @@ public class Maze {
 
 		random = new Random();
 		random.setSeed(seed);
-		
+
 		generateMaze();
 	}
 
 	private void generateMaze() {
+		nodes = new MazeNode[rows][cols];
 		if (start == null || end == null) {
 			for (int i = 0; i < cols; i++) { //
 				for (int j = 0; j < rows; j++) {
-					nodes.add(new MazeNode(i, j));
+					nodes[i][j] = new MazeNode(i, j);
 				}
 			}
-			
-			start = nodes.get(0);
-			end = nodes.get(nodes.size() - 1);
-			// implement a way to make a maze out of DFS
+
+			for (int i = 0; i < cols; i++) { //
+				for (int j = 0; j < rows; j++) {
+					MazeNode curr = nodes[i][j];
+
+					if (nodes[i - 1][j] != null) {
+						curr.addAdjacent(nodes[i - 1][j]); // WEST
+					}
+					if (nodes[i + 1][j] != null) {
+						curr.addAdjacent(nodes[i + 1][j]); // EAST
+					}
+					if (nodes[i][j - 1] != null) {
+						curr.addAdjacent(nodes[i][j - 1]); // SOUTH
+					}
+					if (nodes[i][j + 1] != null) {
+						curr.addAdjacent(nodes[i][j + 1]); // NORTH
+					}
+				}
+			}
+
+			start = nodes[0][0];
+			end = nodes[rows - 1][cols - 1];
+
+			Stack<MazeNode> stack = new Stack<>();
+			int totalCells = rows * cols;
+			MazeNode curr = start;
+			int visitedCells = 1;
+
+			while (visitedCells < totalCells) {
+				ArrayList<MazeNode> neigh = curr.getAdjacencyList();
+				Iterator iter = neigh.iterator();
+				
+				while (iter.hasNext()) {
+					MazeNode nextNode = (MazeNode) iter.next();
+					if (nextNode.getColor() != Color.WHITE) {
+						neigh.remove(nextNode); // THIS PART REMOVES GRAY/VISITED NODES FROM NEIGH
+					}
+				}
+				
+				if (neigh.size() > 0) {
+					curr.setColor(Color.GRAY);
+					int index = random.nextInt(neigh.size() - 1);
+					MazeNode chosen = neigh.get(index);
+					stack.push(curr);
+					curr = chosen;
+					visitedCells++;
+				} else {
+					curr = stack.pop();
+				}
+			}
 		}
 	}
 
 	public String toString() {
-		return ""; // TBD
+		if (nodes != null && nodes.length != 0) {
+			String represent = "";
+			// TBD: MAKE GRID
+			return represent;
+		} else {
+			return "";
+		}
 	}
-	
+
 	public MazeNode getStart() {
 		return start;
 	}
-	
+
 	public MazeNode getEnd() {
 		return end;
 	}
