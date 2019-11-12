ad3e9325f9b2e52526f0d8bbe880e9d7771383d8
sunnyxu2 <sunny.xu@sjsu.edu>
2019-11-12 13:58:16 -0800

    Updated toString method

--- whitehead&xu.cs146.project3/src/maze/Maze.java
+++ whitehead&xu.cs146.project3/src/maze/Maze.java
@@ -1,12 +1,11 @@
 package maze;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Random;
 import java.util.Stack;
 
 /**
- * A maze, represented by a graph object
+ * A maze, represented by a graph object made of MazeNodes
  * 
  * @author Sunny
  *
@@ -49,21 +48,28 @@ public class Maze {
 				}
 			}
 
-			for (int i = 0; i < cols; i++) { //
-				for (int j = 0; j < rows; j++) {
+			for (int i = 0; i < rows; i++) { //
+				for (int j = 0; j < cols; j++) {
 					MazeNode curr = nodes[i][j];
+					MazeNode check;
+					if (i - 1 >= 0) {
+						check = nodes[i - 1][j];
+						curr.addAdjacent(check); // WEST
+					}
 
-					if (nodes[i - 1][j] != null) {
-						curr.addAdjacent(nodes[i - 1][j]); // WEST
+					if (i + 1 < rows) {
+						check = nodes[i + 1][j];
+						curr.addAdjacent(check); // EAST
 					}
-					if (nodes[i + 1][j] != null) {
-						curr.addAdjacent(nodes[i + 1][j]); // EAST
+
+					if (j - 1 >= 0) {
+						check = nodes[i][j - 1];
+						curr.addAdjacent(check); // SOUTH
 					}
-					if (nodes[i][j - 1] != null) {
-						curr.addAdjacent(nodes[i][j - 1]); // SOUTH
-					}
-					if (nodes[i][j + 1] != null) {
-						curr.addAdjacent(nodes[i][j + 1]); // NORTH
+
+					if (j + 1 < cols) {
+						check = nodes[i][j + 1];
+						curr.addAdjacent(check); // NORTH
 					}
 				}
 			}
@@ -77,35 +83,90 @@ public class Maze {
 			int visitedCells = 1;
 
 			while (visitedCells < totalCells) {
-				ArrayList<MazeNode> neigh = curr.getAdjacencyList();
-				Iterator iter = neigh.iterator();
-				
-				while (iter.hasNext()) {
-					MazeNode nextNode = (MazeNode) iter.next();
-					if (nextNode.getColor() != Color.WHITE) {
-						neigh.remove(nextNode); // THIS PART REMOVES GRAY/VISITED NODES FROM NEIGH
+				ArrayList<MazeNode> beigh = curr.getAdjacencyList();
+				ArrayList<MazeNode> neigh = new ArrayList<>();
+
+				for (MazeNode node : beigh) {
+					if (node.getColor() == Color.WHITE && !neigh.contains(node)) {
+						neigh.add(node);
 					}
 				}
-				
+
 				if (neigh.size() > 0) {
 					curr.setColor(Color.GRAY);
-					int index = random.nextInt(neigh.size() - 1);
-					MazeNode chosen = neigh.get(index);
+					MazeNode chosen;
+					int upper = neigh.size() - 1;
+					int index = (int) (random.nextDouble() * neigh.size());
+					chosen = neigh.get(index);
+					curr.addConnected(chosen);
+					chosen.setParent(curr);
 					stack.push(curr);
+					chosen.addConnected(curr);
+					chosen.setColor(Color.GRAY);
 					curr = chosen;
 					visitedCells++;
 				} else {
 					curr = stack.pop();
 				}
 			}
+
+			for (int i = 0; i < cols; i++) { //
+				for (int j = 0; j < rows; j++) {
+					nodes[i][j].setColor(Color.WHITE);
+					;
+				}
+			}
 		}
 	}
 
+	@Override
 	public String toString() {
 		if (nodes != null && nodes.length != 0) {
 			String represent = "";
-			// TBD: MAKE GRID
-			return represent;
+			for (int h = 0; h < rows; h++) {
+				for (int v = 0; v < cols; v++) {
+					MazeNode test = getNode(v, h);
+					if (test.getNorthConnected() != null || test.getCoord().equals(start.getCoord())) {
+						represent += "+ ";
+					} else {
+						represent += "+-";
+					}
+				}
+				represent += "+\n";
+				for (int v = 0; v < cols; v++) {
+					MazeNode test = getNode(v, h);
+					if (test.getWestConnected() != null) {
+						represent += " ";
+					} else {
+						represent += "|";
+					}
+
+					switch (test.getColor()) { // TBD: MODIFY THIS TO DISPLAY A PATH
+						case RED:
+							represent += "#";
+							break;
+						default:
+							represent += " ";
+							break;
+					}
+				}
+				MazeNode test = getNode(cols - 1, h);
+				if (test.getEastConnected() != null) {
+					represent += "";
+				} else {
+					represent += "|";
+				}
+				represent += "\n";
+			}
+			for (int v = 0; v < cols; v++) {
+				MazeNode test = getNode(v, rows - 1);
+				if (test.getSouthConnected() != null || test.getCoord().equals(end.getCoord())) {
+					represent += "+ ";
+				} else {
+					represent += "+-";
+				}
+			}
+			return represent + "+\n";
 		} else {
 			return "";
 		}
@@ -117,5 +178,24 @@ public class Maze {
 
 	public MazeNode getEnd() {
 		return end;
+	}
+
+	public int getWidth() {
+		return rows;
+	}
+
+	public int getHeight() {
+		return cols;
+	}
+
+	public MazeNode getNode(Coord2D coord) {
+		int x = coord.getX();
+		int y = coord.getY();
+
+		return nodes[x][y];
+	}
+
+	public MazeNode getNode(int x, int y) {
+		return nodes[x][y];
 	}
 }
