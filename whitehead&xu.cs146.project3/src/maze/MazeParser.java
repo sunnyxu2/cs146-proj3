dde769db359f9568ce93e5c7284a416efd194ac1
sunnyxu2 <sunny.xu@sjsu.edu>
2019-11-22 23:44:12 -0800

    added TBD comments for later

--- whitehead&xu.cs146.project3/src/maze/MazeParser.java
+++ whitehead&xu.cs146.project3/src/maze/MazeParser.java
@@ -27,24 +27,50 @@ public class MazeParser {
 				int xc = (hor - 1) / 2;
 				int yc = (ver - 1) / 2;
 				System.out.print("(" + xc + ", " + yc + ")");
+				if (yc == 0) { // topmost row, ignore the top wall
+					// TBD
+				} else if (yc == y - 1) { // bottom most row, ignore the bottom wall
+					// TBD
+				} else { // do not ignore the walls above and below
+					if ((grid[ver - 1][hor] + "").equals("-")) { // check top wall
+						// TBD: wall
+					} else {
+						// TBD: connect the top npde to this node
+					}
+
+					if (xc == 0) { // leftmost column, ignore the left wall
+						// TBD: check right wall
+					} else if (xc == x - 1) { // rightmost column, ignore the right wall
+						// TBD: check left wall
+					} else { // do not ignore the right and left walls
+						if ((grid[ver][hor - 1] + "").equals("|")) { // check left wall
+							// TBD: wall
+						} else {
+							// TBD: connect the left node to this node
+						}
+
+						if ((grid[ver][hor + 1] + "").equals("|")) { // check right wall
+							// TBD: wall
+						} else {
+							// TBD: connect the right node to this node
+						}
+					}
+
+					if ((grid[ver + 1][hor] + "").equals("-")) { // check bottom wall
+						// TBD: wall
+					} else {
+						// TBD: connect the bottom node to this node
+					}
+				}
 			}
 			System.out.println();
 		}
 		return maze;
 	}
 
-	public static void main (String[] args) {
-		String test = "4 4\r\n" + 
-				"+ +-+-+-+\r\n" + 
-				"|   |   |\r\n" + 
-				"+-+ +-+ +\r\n" + 
-				"| |     |\r\n" + 
-				"+ +-+-+ +\r\n" + 
-				"|       |\r\n" + 
-				"+ +-+-+-+\r\n" + 
-				"|       |\r\n" + 
-				"+-+-+-+ +\r\n" + 
-				"";
+	public static void main(String[] args) {
+		String test = "4 4\r\n" + "+ +-+-+-+\r\n" + "|   |   |\r\n" + "+-+ +-+ +\r\n" + "| |     |\r\n"
+				+ "+ +-+-+ +\r\n" + "|       |\r\n" + "+ +-+-+-+\r\n" + "|       |\r\n" + "+-+-+-+ +\r\n" + "";
 		MazeParser.parse(test);
 	}
 }
