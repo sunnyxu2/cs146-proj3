097674e54a1ad128e427de44ee1705ec0d3ecb55
sunnyxu2 <sunny.xu@sjsu.edu>
2019-11-22 23:02:13 -0800

    Updated class comment to attribute to both contributors, also formatting

--- whitehead&xu.cs146.project3/src/maze/MazeSolver.java
+++ whitehead&xu.cs146.project3/src/maze/MazeSolver.java
@@ -7,16 +7,13 @@ import java.util.Stack;
 /**
  * Class that solves mazes
  * 
- * @author Sunny
- *
- */
-/**
- * @author williamwhitehead
+ * @author Sunny and williamwhitehead
  *
  */
 public class MazeSolver {
 	/**
 	 * Traverses the maze using breadth-first search
+	 * 
 	 * @param maze
 	 * @param source
 	 * @return String representation of the maze
@@ -38,8 +35,7 @@ public class MazeSolver {
 		queue.add(source);
 		source.setLabel("0");
 		int counter = 0;
-		outer:
-		while (!queue.isEmpty()) {
+		outer: while (!queue.isEmpty()) {
 			MazeNode you = queue.remove();
 			for (MazeNode adj : you.getConnectedList()) {
 				if (adj.getColor() == Color.WHITE) {
@@ -47,7 +43,8 @@ public class MazeSolver {
 					adj.setColor(Color.GRAY);
 					adj.setDistance(you.getDistance() + 1);
 					adj.setParent(you);
-					adj.setLabel(Integer.toString(counter % 10)); //adj.setLabel(Integer.toString(adj.getDistance() % 10));
+					adj.setLabel(Integer.toString(counter % 10)); // adj.setLabel(Integer.toString(adj.getDistance() %
+																	// 10));
 					queue.add(adj);
 					if (adj.equals(maze.getEnd())) {
 						break outer;
@@ -57,9 +54,10 @@ public class MazeSolver {
 			you.setColor(Color.BLACK);
 		}
 	}
-	
+
 	/**
 	 * Traverses a solved maze to find the path from start to end
+	 * 
 	 * @param maze
 	 * @return true if the maze has been solved, false otherwise
 	 */
@@ -90,41 +88,41 @@ public class MazeSolver {
 			return false;
 		}
 	}
-	
 
 	/**
 	 * Returns path and cell statistics for a traversed maze
-	 * @param coords	a stack of visited cell coordinates
-	 * @param maze		the traversed maze
+	 * 
+	 * @param coords a stack of visited cell coordinates
+	 * @param maze   the traversed maze
 	 */
-	public static void pathStats(Stack<Coord2D> coords, Maze maze)
-	{
+	public static void pathStats(Stack<Coord2D> coords, Maze maze) {
 		System.out.print("Path: ");
 		int pathCounter = 0;
-		while(!coords.isEmpty()) {
-		System.out.print(coords.pop().toString()+", ");
-		pathCounter++;
-		if(pathCounter%10 == 0)	System.out.println("");
+		while (!coords.isEmpty()) {
+			System.out.print(coords.pop().toString() + ", ");
+			pathCounter++;
+			if (pathCounter % 10 == 0)
+				System.out.println("");
 		}
 		System.out.println();
 		System.out.println("Length of path: " + pathCounter);
-		
-		int visitCounter =0;
+
+		int visitCounter = 0;
 		for (int i = 0; i < maze.getWidth(); i++) {
 			for (int j = 0; j < maze.getHeight(); j++) {
-				if (maze.getNode(i, j).getColor() == Color.GRAY || 
-				    maze.getNode(i, j).getColor() == Color.BLACK) {
+				if (maze.getNode(i, j).getColor() == Color.GRAY || maze.getNode(i, j).getColor() == Color.BLACK) {
 					visitCounter++;
 				}
 			}
 		}
 		System.out.println("Visited cells: " + visitCounter);
 		System.out.println();
-		
+
 	}
-	
+
 	/**
 	 * Traverses the maze using depth-first search
+	 * 
 	 * @param maze
 	 * @param source
 	 */
@@ -143,31 +141,28 @@ public class MazeSolver {
 				u.setLabel(" ");
 			}
 		}
-			source.setDistance(0);
-			source.setParent(null);
-			Stack<MazeNode> stack = new Stack<MazeNode>();
-			stack.add(source);
-			source.setLabel("0");
-			int counter = 0;
-			while(!stack.isEmpty())
-			{
-				MazeNode u = stack.pop();
-				if(u.getColor() == Color.WHITE)
-				{
-					u.setColor(Color.GRAY);
-					if (u.equals(maze.getEnd())) break;
-					for(MazeNode adj : u.getConnectedList())
-					{
-						if(adj.getColor() == Color.WHITE)
-						{
-							stack.push(adj);
-							adj.setParent(u);
-							counter++;
-							adj.setLabel(Integer.toString(counter%10));
-						}
+		source.setDistance(0);
+		source.setParent(null);
+		Stack<MazeNode> stack = new Stack<MazeNode>();
+		stack.add(source);
+		source.setLabel("0");
+		int counter = 0;
+		while (!stack.isEmpty()) {
+			MazeNode u = stack.pop();
+			if (u.getColor() == Color.WHITE) {
+				u.setColor(Color.GRAY);
+				if (u.equals(maze.getEnd()))
+					break;
+				for (MazeNode adj : u.getConnectedList()) {
+					if (adj.getColor() == Color.WHITE) {
+						stack.push(adj);
+						adj.setParent(u);
+						counter++;
+						adj.setLabel(Integer.toString(counter % 10));
 					}
-					u.setColor(Color.BLACK);
 				}
+				u.setColor(Color.BLACK);
 			}
+		}
 	}
 }
