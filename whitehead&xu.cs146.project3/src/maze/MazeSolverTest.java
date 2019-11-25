4738670ed61cca3f16eeadb7cf2fc027d90e16b4
sunnyxu2 <sunny.xu@sjsu.edu>
2019-11-24 16:25:56 -0800

    put some example ascii tests

--- whitehead&xu.cs146.project3/src/maze/MazeSolverTest.java
+++ whitehead&xu.cs146.project3/src/maze/MazeSolverTest.java
@@ -22,8 +22,223 @@ class MazeSolverTest {
 		System.out.println(maze.toString());
 		
 		//now to test potika's mazes
+		String t44 = "4 4\n" + 
+				"+ +-+-+-+\n" + 
+				"|   |   |\n" + 
+				"+-+ +-+ +\n" + 
+				"| |     |\n" + 
+				"+ +-+-+ +\n" + 
+				"|       |\n" + 
+				"+ +-+-+-+\n" + 
+				"|       |\n" + 
+				"+-+-+-+ +";
+		Maze test44 = MazeParser.parse(t44);
+		System.out.println(test44.getWidth() + " " + test44.getHeight() + "\n" + test44.toString());
+		assertEquals(test44.toString(), 
+				"+ +-+-+-+\n" + 
+				"|   |   |\n" + 
+				"+-+ +-+ +\n" + 
+				"| |     |\n" + 
+				"+ +-+-+ +\n" + 
+				"|       |\n" + 
+				"+ +-+-+-+\n" + 
+				"|       |\n" + 
+				"+-+-+-+ +\n");
+		System.out.println(test44.getWidth() + " " + test44.getHeight() + "\n" + test44.toString());
+		MazeSolver.solve_BFS(test44, test44.getStart());
+		System.out.println("solve_BFS: \n" + test44.toString());
+		MazeSolver.traverse(test44);
+		System.out.println(test44.toString());
 		
+		MazeSolver.solve_DFS(test44, test44.getStart());
+		System.out.println("solve_DFS: \n" + test44.toString());
+		MazeSolver.traverse(test44);
+		System.out.println(test44.toString());
 		
+		String t66 = "6 6\r\n" + 
+				"+ +-+-+-+-+-+\r\n" + 
+				"|   |       |\r\n" + 
+				"+-+ +-+-+-+ +\r\n" + 
+				"| |       | |\r\n" + 
+				"+ +-+-+-+ + +\r\n" + 
+				"|     | |   |\r\n" + 
+				"+ +-+ + +-+ +\r\n" + 
+				"| |       | |\r\n" + 
+				"+ +-+-+-+-+ +\r\n" + 
+				"|     |     |\r\n" + 
+				"+ +-+ + +-+-+\r\n" + 
+				"|   |       |\r\n" + 
+				"+-+-+-+-+-+ +";
+		Maze test66 = MazeParser.parse(t66);
+		System.out.println(test66.getWidth() + " " + test66.getHeight() + "\n" + test66.toString());
+		assertEquals(test66.toString(), 
+				"+ +-+-+-+-+-+\n" + 
+				"|   |       |\n" + 
+				"+-+ +-+-+-+ +\n" + 
+				"| |       | |\n" + 
+				"+ +-+-+-+ + +\n" + 
+				"|     | |   |\n" + 
+				"+ +-+ + +-+ +\n" + 
+				"| |       | |\n" + 
+				"+ +-+-+-+-+ +\n" + 
+				"|     |     |\n" + 
+				"+ +-+ + +-+-+\n" + 
+				"|   |       |\n" + 
+				"+-+-+-+-+-+ +\n");
+		System.out.println(test66.getWidth() + " " + test66.getHeight() + "\n" + test66.toString());
+		MazeSolver.solve_BFS(test66, test66.getStart());
+		System.out.println("solve_BFS: \n" + test66.toString());
+		MazeSolver.traverse(test66);
+		System.out.println(test66.toString());
+		
+		MazeSolver.solve_DFS(test66, test66.getStart());
+		System.out.println("solve_DFS: \n" + test66.toString());
+		MazeSolver.traverse(test66);
+		System.out.println(test66.toString());
+		
+		String t88 = "8 8\r\n" + 
+				"+ +-+-+-+-+-+-+-+\r\n" + 
+				"|   |         | |\r\n" + 
+				"+-+ +-+-+-+ + + +\r\n" + 
+				"| |       | |   |\r\n" + 
+				"+ +-+-+-+ + +-+ +\r\n" + 
+				"|   |   |   |   |\r\n" + 
+				"+ + + + +-+-+ +-+\r\n" + 
+				"| |   | |     | |\r\n" + 
+				"+ +-+-+-+ +-+-+ +\r\n" + 
+				"|         |   | |\r\n" + 
+				"+ +-+-+-+-+ + + +\r\n" + 
+				"| |     |   | | |\r\n" + 
+				"+ + +-+ + +-+ + +\r\n" + 
+				"| | | | | |   | |\r\n" + 
+				"+ + + + + + +-+ +\r\n" + 
+				"|   |     |     |\r\n" + 
+				"+-+-+-+-+-+-+-+ +";
+		Maze test88 = MazeParser.parse(t88);
+		System.out.println(test88.getWidth() + " " + test88.getHeight() + "\n" + test88.toString());
+		assertEquals(test88.toString(), 
+				"+ +-+-+-+-+-+-+-+\n" + 
+				"|   |         | |\n" + 
+				"+-+ +-+-+-+ + + +\n" + 
+				"| |       | |   |\n" + 
+				"+ +-+-+-+ + +-+ +\n" + 
+				"|   |   |   |   |\n" + 
+				"+ + + + +-+-+ +-+\n" + 
+				"| |   | |     | |\n" + 
+				"+ +-+-+-+ +-+-+ +\n" + 
+				"|         |   | |\n" + 
+				"+ +-+-+-+-+ + + +\n" + 
+				"| |     |   | | |\n" + 
+				"+ + +-+ + +-+ + +\n" + 
+				"| | | | | |   | |\n" + 
+				"+ + + + + + +-+ +\n" + 
+				"|   |     |     |\n" + 
+				"+-+-+-+-+-+-+-+ +\n");
+		System.out.println(test88.getWidth() + " " + test88.getHeight() + "\n" + test88.toString());
+		MazeSolver.solve_BFS(test88, test88.getStart());
+		System.out.println("solve_BFS: \n" + test88.toString());
+		MazeSolver.traverse(test88);
+		System.out.println(test88.toString());
+		
+		MazeSolver.solve_DFS(test88, test88.getStart());
+		System.out.println("solve_DFS: \n" + test88.toString());
+		MazeSolver.traverse(test88);
+		System.out.println(test88.toString());
+		
+		String t2020 = "20 20\r\n" + 
+				"+ +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\r\n" + 
+				"| |     |   |       | | |     |     |   |\r\n" + 
+				"+ + + + + +-+-+-+ + + + +-+ +-+-+ +-+-+ +\r\n" + 
+				"|   | |   |   |   |   | | |       |     |\r\n" + 
+				"+-+-+ +-+ + + +-+ + +-+ + + +-+-+ + + + +\r\n" + 
+				"|   | |   | | |   |   |         |   | | |\r\n" + 
+				"+-+ +-+-+ + +-+ +-+-+ +-+-+-+-+-+ + +-+ +\r\n" + 
+				"| | |             | |     |       | |   |\r\n" + 
+				"+ + + +-+ +-+-+-+-+ +-+ +-+-+ +-+-+ +-+-+\r\n" + 
+				"| |     |   |     |           |   | |   |\r\n" + 
+				"+ +-+-+ +-+-+-+ +-+-+-+ + +-+-+ +-+-+ +-+\r\n" + 
+				"| |   |     | |         |   | | |   |   |\r\n" + 
+				"+ +-+ + + +-+ + +-+-+-+ +-+-+ + + +-+-+ +\r\n" + 
+				"|   | | |   |       | | |     |         |\r\n" + 
+				"+ +-+ +-+-+-+ +-+-+-+ +-+ +-+ + +-+ +-+-+\r\n" + 
+				"|     | | |   |   | |   | |   | | | |   |\r\n" + 
+				"+-+ + + + + +-+-+ + +-+ + +-+ + + + + +-+\r\n" + 
+				"|   |   |                 | |   |     | |\r\n" + 
+				"+-+ +-+ + +-+ +-+-+-+ +-+-+ +-+ + +-+-+ +\r\n" + 
+				"|   |     | |   |           |   | |     |\r\n" + 
+				"+-+-+ + +-+ +-+-+ +-+-+ + +-+-+ + +-+ + +\r\n" + 
+				"| |   | |   |     | |   | |     | |   | |\r\n" + 
+				"+ + +-+-+ +-+ +-+ + +-+-+ +-+-+-+ +-+ +-+\r\n" + 
+				"|   | |       |   | |         | |   | | |\r\n" + 
+				"+ +-+ + +-+ +-+ +-+ +-+ + +-+-+ +-+-+ + +\r\n" + 
+				"| |       |   |   |   | | |       | |   |\r\n" + 
+				"+ + + +-+-+ +-+-+-+ +-+-+ + +-+-+ + + +-+\r\n" + 
+				"| | | | |   |   |   | | | |   |       | |\r\n" + 
+				"+-+-+-+ +-+ +-+ +-+ + + + + +-+-+ +-+ + +\r\n" + 
+				"| |   |           | |         |     |   |\r\n" + 
+				"+ + + +-+-+-+-+ + + + +-+ +-+ +-+ +-+ + +\r\n" + 
+				"| | |           | |   | |   |   |   | | |\r\n" + 
+				"+ +-+ +-+-+ + +-+ + + + + +-+ +-+-+ + +-+\r\n" + 
+				"|     | |   | | | | | |   | | | |   | | |\r\n" + 
+				"+ + + + +-+-+ + + +-+-+-+-+ + + + +-+-+ +\r\n" + 
+				"| | | |   |     | |           | |   |   |\r\n" + 
+				"+ +-+ + + + +-+ +-+ +-+-+-+ + + +-+-+ + +\r\n" + 
+				"|   | | |   |     | |       |         | |\r\n" + 
+				"+-+ + + + + +-+-+ +-+-+ +-+ + + +-+-+ + +\r\n" + 
+				"|   | | | |   |     |   |   | | |     | |\r\n" + 
+				"+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ +";
+		Maze test2020 = MazeParser.parse(t2020);
+		System.out.println(test2020.getWidth() + " " + test2020.getHeight() + "\n" + test2020.toString());
+		assertEquals(test2020.toString(), "+ +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n" + 
+				"| |     |   |       | | |     |     |   |\n" + 
+				"+ + + + + +-+-+-+ + + + +-+ +-+-+ +-+-+ +\n" + 
+				"|   | |   |   |   |   | | |       |     |\n" + 
+				"+-+-+ +-+ + + +-+ + +-+ + + +-+-+ + + + +\n" + 
+				"|   | |   | | |   |   |         |   | | |\n" + 
+				"+-+ +-+-+ + +-+ +-+-+ +-+-+-+-+-+ + +-+ +\n" + 
+				"| | |             | |     |       | |   |\n" + 
+				"+ + + +-+ +-+-+-+-+ +-+ +-+-+ +-+-+ +-+-+\n" + 
+				"| |     |   |     |           |   | |   |\n" + 
+				"+ +-+-+ +-+-+-+ +-+-+-+ + +-+-+ +-+-+ +-+\n" + 
+				"| |   |     | |         |   | | |   |   |\n" + 
+				"+ +-+ + + +-+ + +-+-+-+ +-+-+ + + +-+-+ +\n" + 
+				"|   | | |   |       | | |     |         |\n" + 
+				"+ +-+ +-+-+-+ +-+-+-+ +-+ +-+ + +-+ +-+-+\n" + 
+				"|     | | |   |   | |   | |   | | | |   |\n" + 
+				"+-+ + + + + +-+-+ + +-+ + +-+ + + + + +-+\n" + 
+				"|   |   |                 | |   |     | |\n" + 
+				"+-+ +-+ + +-+ +-+-+-+ +-+-+ +-+ + +-+-+ +\n" + 
+				"|   |     | |   |           |   | |     |\n" + 
+				"+-+-+ + +-+ +-+-+ +-+-+ + +-+-+ + +-+ + +\n" + 
+				"| |   | |   |     | |   | |     | |   | |\n" + 
+				"+ + +-+-+ +-+ +-+ + +-+-+ +-+-+-+ +-+ +-+\n" + 
+				"|   | |       |   | |         | |   | | |\n" + 
+				"+ +-+ + +-+ +-+ +-+ +-+ + +-+-+ +-+-+ + +\n" + 
+				"| |       |   |   |   | | |       | |   |\n" + 
+				"+ + + +-+-+ +-+-+-+ +-+-+ + +-+-+ + + +-+\n" + 
+				"| | | | |   |   |   | | | |   |       | |\n" + 
+				"+-+-+-+ +-+ +-+ +-+ + + + + +-+-+ +-+ + +\n" + 
+				"| |   |           | |         |     |   |\n" + 
+				"+ + + +-+-+-+-+ + + + +-+ +-+ +-+ +-+ + +\n" + 
+				"| | |           | |   | |   |   |   | | |\n" + 
+				"+ +-+ +-+-+ + +-+ + + + + +-+ +-+-+ + +-+\n" + 
+				"|     | |   | | | | | |   | | | |   | | |\n" + 
+				"+ + + + +-+-+ + + +-+-+-+-+ + + + +-+-+ +\n" + 
+				"| | | |   |     | |           | |   |   |\n" + 
+				"+ +-+ + + + +-+ +-+ +-+-+-+ + + +-+-+ + +\n" + 
+				"|   | | |   |     | |       |         | |\n" + 
+				"+-+ + + + + +-+-+ +-+-+ +-+ + + +-+-+ + +\n" + 
+				"|   | | | |   |     |   |   | | |     | |\n" + 
+				"+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ +\n");
+		System.out.println(test2020.getWidth() + " " + test2020.getHeight() + "\n" + test2020.toString());
+		MazeSolver.solve_BFS(test2020, test2020.getStart());
+		System.out.println("solve_BFS: \n" + test2020.toString());
+		MazeSolver.traverse(test2020);
+		System.out.println(test2020.toString());
+		
+		MazeSolver.solve_DFS(test2020, test2020.getStart());
+		System.out.println("solve_DFS: \n" + test2020.toString());
+		MazeSolver.traverse(test2020);
+		System.out.println(test2020.toString());
 	}
-
 }
